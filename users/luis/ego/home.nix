b523623f7be5e3d64abe4e639a2087ae44a2ba30
luis <ldesiqueira@pinger.com>
2024-05-11 14:48:40 -0300

    ego home configuration

--- users/luis/ego/home.nix
+++ users/luis/ego/home.nix
@@ -1,25 +1,22 @@
 { outputs, pkgs, ... }:
 {
-  home.stateVersion = "22.11";
-  home.username = "luis";
-  home.homeDirectory = "/home/luis";
-  # imports = [
-  #   outputs.homeManagerModules.blackmatter
-  #   ../../luis/common
-  #   ../common
-  # ];
-  # nixpkgs.overlays = [
-  #   (self: super: {
-  #     fcitx-engines = self.fcitx5;
-  #   })
-  # ];
-  # blackmatter.programs.nvim.enable = true;
-  # blackmatter.shell.enable = true;
-  # blackmatter.desktop.enable = false;
-  # blackmatter.desktop.alacritty.config.enable = true;
-  # nixpkgs.config.allowUnfree = true;
-  # nixpkgs.config.allowBroken = true;
-  # nixpkgs.config.permittedInsecurePackages = [
-  #   "electron-25.9.0"
-  # ];
+  imports = [
+    outputs.homeManagerModules.blackmatter
+    ../../luis/common
+    ../common
+  ];
+  nixpkgs.overlays = [
+    (self: super: {
+      fcitx-engines = self.fcitx5;
+    })
+  ];
+  blackmatter.programs.nvim.enable = true;
+  blackmatter.shell.enable = true;
+  blackmatter.desktop.enable = false;
+  blackmatter.desktop.alacritty.config.enable = false;
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowBroken = true;
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-25.9.0"
+  ];
 }
